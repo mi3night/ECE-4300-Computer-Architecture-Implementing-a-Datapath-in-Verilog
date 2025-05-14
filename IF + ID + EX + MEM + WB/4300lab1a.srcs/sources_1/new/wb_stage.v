954e7606263c32d9703989c0fcc4cfb2d682cce8
mi3night <122961945+mi3night@users.noreply.github.com>
2025-05-14 00:56:44 -0700

    fix

--- IF + ID + EX + MEM + WB/4300lab1a.srcs/sources_1/new/wb_stage.v
+++ IF + ID + EX + MEM + WB/4300lab1a.srcs/sources_1/new/wb_stage.v
@@ -1,19 +1,21 @@
-module wb_stage (
-    input [1:0]  mem_wb_wb_ctl,
-    input [31:0] mem_wb_mem_read_data,
-    input [31:0] mem_wb_alu_result,
-    input [4:0]  mem_wb_write_reg,
+`timescale 1ns/1ps
 
-    output       regwrite_wb,
-    output [4:0] write_reg_wb,
+module wb_stage (
+    input  [1:0]  mem_wb_wb_ctl,
+    input  [31:0] mem_wb_mem_read_data,
+    input  [31:0] mem_wb_alu_result,
+    input  [4:0]  mem_wb_write_reg,
+
+    output        regwrite_wb,
+    output [4:0]  write_reg_wb,
     output [31:0] write_data_wb
 );
 
-    wire regwrite   = mem_wb_wb_ctl[1];
-    wire memtoreg   = mem_wb_wb_ctl[0];
+    wire regwrite  = mem_wb_wb_ctl[1];  // RegWrite
+    wire memtoreg  = mem_wb_wb_ctl[0];  // MemtoReg
 
-    assign regwrite_wb    = regwrite;
-    assign write_reg_wb   = mem_wb_write_reg;
-    assign write_data_wb  = memtoreg ? mem_wb_mem_read_data : mem_wb_alu_result;
+    assign regwrite_wb   = regwrite;
+    assign write_reg_wb  = mem_wb_write_reg;
+    assign write_data_wb = memtoreg ? mem_wb_mem_read_data : mem_wb_alu_result;
 
 endmodule
